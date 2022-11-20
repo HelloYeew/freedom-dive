bbec9757a9fa53efa119827bffd165e5c6ba9fac
Phawit Pornwattanakul <me@helloyeew.dev>
2022-11-20 20:11:56 +0700

    Add import beatmap from via API

--- users/models.py
+++ users/models.py
@@ -8,7 +8,7 @@ class ColourSettings(models.Model):
     color_primary = ColorField(default='#DFD9D6')
     color_accent = ColorField(default='#DBC2D1')
     color_background = ColorField(default='#0A0A0A')
-    mask_opacity = models.FloatField(default=0.5, max_length=1)
+    mask_opacity = models.FloatField(default=0.7, max_length=1)
 
     def __str__(self):
         return self.user.username + '\'s colour settings'
--- utility/models.py
+++ utility/models.py
@@ -11,3 +11,13 @@ class UtilityLog(models.Model):
 
     def __str__(self):
         return f'{self.user} - {self.field} - {self.time}'
+
+
+class ImportBeatmapsetUsageLog(models.Model):
+    time = models.DateTimeField(auto_now_add=True)
+    beatmapset_id = models.IntegerField()
+    success = models.BooleanField(default=False)
+    description = models.TextField(default='')
+
+    def __str__(self):
+        return f'{self.beatmapset_id} - {self.time} - {self.success}'
