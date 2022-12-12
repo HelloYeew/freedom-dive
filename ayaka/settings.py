5feb649268e652c3690a13d232a8f2987c387078
Phawit Pornwattanakul <me@helloyeew.dev>
2022-12-13 03:52:54 +0700

    Fix sentry DSN always reading blank string as DSN

--- ayaka/settings.py
+++ ayaka/settings.py
@@ -172,18 +172,19 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # Sentry SDK configuration
-sentry_sdk.init(
-    dsn=config('SENTRY_DSN', default=''),
-    integrations=[
-        DjangoIntegration(),
-    ],
+if config('SENTRY_DSN', default='') != '':
+    sentry_sdk.init(
+        dsn=config('SENTRY_DSN', default=''),
+        integrations=[
+            DjangoIntegration(),
+        ],
 
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
 
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
-)
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True
+    )
