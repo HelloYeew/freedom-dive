5e7425c7efeb6d0ba451eb174f48587cb97b1cbc
Phawit Pornwattanakul <me@helloyeew.dev>
2022-12-15 18:30:55 +0700

    Fix admin page not render markdown

--- apps/admin.py
+++ apps/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
+from mdeditor.widgets import MDEditorWidget
 
 from apps.models import *
 
+class ClientChangelogAdmin (admin.ModelAdmin):
+    formfield_overrides = {
+        models.TextField: {'widget': MDEditorWidget}
+    }
+
 admin.site.register(ScoreStore)
-admin.site.register(ClientChangelog)
+admin.site.register(ClientChangelog, ClientChangelogAdmin)
