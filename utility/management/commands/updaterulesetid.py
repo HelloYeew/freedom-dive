256ec6e52af2c5ba5a0b1f8cd737e8542c4baf0d
Phawit Pornwattanakul <me@helloyeew.dev>
2023-02-03 12:34:13 +0700

    Add try except to update ruleset id operation

--- utility/management/commands/updaterulesetid.py
+++ utility/management/commands/updaterulesetid.py
@@ -10,8 +10,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS(f'Transferring score from legacy store to new store...'))
         for score in ScoreStore.objects.all():
-            score.data['ruleset_id'] = get_ruleset_id(score.ruleset_short_name)
-            print("Updating score ID: " + score.score_id + " with ruleset ID: " + str(score.data['ruleset_id']))
-            print(score.data)
-            score.save()
+            try:
+                score.data['ruleset_id'] = get_ruleset_id(score.ruleset_short_name)
+                print("Updating score ID: " + score.score_id + " with ruleset ID: " + str(score.data['ruleset_id']))
+                print(score.data)
+                score.save()
+            except Exception as e:
+                print("Error updating score ID: " + score.score_id)
+                print(e)
         self.stdout.write(self.style.SUCCESS(f'Finished transferring score from legacy store to new store'))
